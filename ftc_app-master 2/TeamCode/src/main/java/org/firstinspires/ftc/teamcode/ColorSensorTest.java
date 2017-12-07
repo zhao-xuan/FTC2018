35a74bb718af1fa0aa08e0eafddaf262d09b4285
zhaoxuan <1418756682@qq.com>
2017-12-06 16:49:09 -0800

    change

---
+++ ftc_app-master 2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * Created by 14187 on 12/6/2017.
+ */
+
+
+public class ColorSensorTest extends LinearOpMode {
+
+    public ColorSensor color_sensor;
+    public int r = color_sensor.red();
+    public int g = color_sensor.green();
+    public int b = color_sensor.blue();
+    public int a = color_sensor.alpha();
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Test Mode state");
+        telemetry.update();
+    }
+
+}
