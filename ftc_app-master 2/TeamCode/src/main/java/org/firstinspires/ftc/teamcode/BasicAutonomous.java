b08e7785dd0cb3cac8e17750e4b7a2d176ef81cc
LunaYoYuki <ywang18@sandomenico.org>
2018-01-08 19:44:07 -0800

    Autonomous
    不太work的

---
+++ ftc_app-master 2/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicAutonomous.java
@@ -0,0 +1,260 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
+import com.qualcomm.robotcore.hardware.NormalizedRGBA;
+import com.qualcomm.robotcore.hardware.SwitchableLight;
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Basic Autonomous", group="Linear Opmode")
+//@Disabled
+public class BasicAutonomous extends LinearOpMode {
+
+    // Declare OpMode members.
+
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrivef;
+    private DcMotor rightDrivef;
+    private DcMotor leftDriveb;
+    private DcMotor rightDriveb;
+
+    NormalizedColorSensor colorSensor;
+    View relativeLayout;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Initialize the hardware variables. Note that the strings used here as parameters
+        // to 'get' must correspond to the names assigned during the robot configuration
+        // step (using the FTC Robot Controller app on the phone).
+        leftDrivef  = hardwareMap.get(DcMotor.class, "left_drivef");
+        rightDrivef = hardwareMap.get(DcMotor.class, "right_drivef");
+        leftDriveb  = hardwareMap.get(DcMotor.class, "left_driveb");
+        rightDriveb = hardwareMap.get(DcMotor.class, "right_driveb");
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrivef.setDirection(DcMotor.Direction.FORWARD);
+        rightDrivef.setDirection(DcMotor.Direction.REVERSE);
+        leftDriveb.setDirection(DcMotor.Direction.FORWARD);
+        rightDriveb.setDirection(DcMotor.Direction.REVERSE);
+
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            driveStraight(1);
+            sleep(5000); //for 5 seconds
+            turn(1);
+            sleep(2000);
+            stopDriving();
+
+            try {
+                runSample(); // actually execute the sample
+            } finally {
+                // On the way out, *guarantee* that the background is reasonable. It doesn't actually start off
+                // as pure white, but it's too much work to dig out what actually was used, and this is good
+                // enough to at least make the screen reasonable again.
+                // Set the panel back to the default color
+                relativeLayout.post(new Runnable() {
+                    public void run() {
+                        relativeLayout.setBackgroundColor(Color.WHITE);
+                    }
+                });
+            }
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+
+    public void driveStraight (double power){
+        //double r = Math.abs (power);
+        //double robotAngle = Math.atan2(-power, 0) - Math.PI/4;
+        //Math.atan2(-power, 0) == 0
+
+        //final double v1 = Math.abs(power) * Math.cos(-Math.PI/4);
+        //final double v2 = Math.abs(power) * Math.sin(-Math.PI/4);
+        //final double v3 = Math.abs(power) * Math.sin(-Math.PI/4);
+        //final double v4 = Math.abs(power) * Math.cos(-Math.PI/4);
+        //or maybe just set their power to 1?
+        leftDrivef.setPower(power);
+        rightDrivef.setPower(power);
+        leftDriveb.setPower(power);
+        rightDriveb.setPower(power);
+    }
+
+    public void stopDriving (){
+        driveStraight (0);
+    }
+
+    public void turn (double power) {
+        //double r = Math.hypot(-gamepad1.right_stick_x, gamepad1.left_stick_y); //check if we need to make "gamepad1.left_stick_y" negative"
+        //double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+        //double rightX = -gamepad1.right_stick_x;
+        final double v1 = Math.abs(power) * Math.cos(-Math.PI/4) + power;
+        final double v2 = Math.abs(power) * Math.sin(-Math.PI/4) - power;
+        final double v3 = Math.abs(power) * Math.sin(-Math.PI/4) + power;
+        final double v4 = Math.abs(power) * Math.cos(-Math.PI/4) - power;
+
+        leftDrivef.setPower(v1);
+        rightDrivef.setPower(v2);
+        leftDriveb.setPower(v3);
+        rightDriveb.setPower(v4);
+    }
+
+    protected void runSample() throws InterruptedException {
+
+        // values is a reference to the hsvValues array.
+        float[] hsvValues = new float[3];
+        final float values[] = hsvValues;
+
+        // bPrevState and bCurrState keep track of the previous and current state of the button
+        boolean bPrevState = false;
+        boolean bCurrState = false;
+
+        // Get a reference to our sensor object.
+        colorSensor = hardwareMap.get(NormalizedColorSensor.class, "sensor_color");
+
+        // If possible, turn the light on in the beginning (it might already be on anyway,
+        // we just make sure it is if we can).
+        if (colorSensor instanceof SwitchableLight) {
+            ((SwitchableLight) colorSensor).enableLight(true);
+        }
+
+        // Wait for the start button to be pressed.
+        waitForStart();
+
+        // Loop until we are asked to stop
+        while (opModeIsActive()) {
+
+            // Read the sensor
+            NormalizedRGBA colors = colorSensor.getNormalizedColors();
+
+            /** Use telemetry to display feedback on the driver station. We show the conversion
+             * of the colors to hue, saturation and value, and display the the normalized values
+             * as returned from the sensor.
+             * @see <a href="http://infohost.nmt.edu/tcc/help/pubs/colortheory/web/hsv.html">HSV</a>*/
+
+            Color.colorToHSV(colors.toColor(), hsvValues);
+            if (hsvValues[0] <= 230 && hsvValues[0] >= 200) {
+                //blue jewel
+                driveStraight(1);
+            } else if (hsvValues[0] <= 5 || hsvValues[0] >= 350) {
+                //red jewel
+                driveStraight(-1);
+            }
+
+            telemetry.addLine()
+                    .addData("H", "%.3f", hsvValues[0])
+                    .addData("S", "%.3f", hsvValues[1])
+                    .addData("V", "%.3f", hsvValues[2]);
+            telemetry.addLine()
+                    .addData("a", "%.3f", colors.alpha)
+                    .addData("r", "%.3f", colors.red)
+                    .addData("g", "%.3f", colors.green)
+                    .addData("b", "%.3f", colors.blue);
+
+            /** We also display a conversion of the colors to an equivalent Android color integer.
+             * @see Color */
+            int color = colors.toColor();
+            telemetry.addLine("raw Android color: ")
+                    .addData("a", "%02x", Color.alpha(color))
+                    .addData("r", "%02x", Color.red(color))
+                    .addData("g", "%02x", Color.green(color))
+                    .addData("b", "%02x", Color.blue(color));
+
+            // Balance the colors. The values returned by getColors() are normalized relative to the
+            // maximum possible values that the sensor can measure. For example, a sensor might in a
+            // particular configuration be able to internally measure color intensity in a range of
+            // [0, 10240]. In such a case, the values returned by getColors() will be divided by 10240
+            // so as to return a value it the range [0,1]. However, and this is the point, even so, the
+            // values we see here may not get close to 1.0 in, e.g., low light conditions where the
+            // sensor measurements don't approach their maximum limit. In such situations, the *relative*
+            // intensities of the colors are likely what is most interesting. Here, for example, we boost
+            // the signal on the colors while maintaining their relative balance so as to give more
+            // vibrant visual feedback on the robot controller visual display.
+            float max = Math.max(Math.max(Math.max(colors.red, colors.green), colors.blue), colors.alpha);
+            colors.red /= max;
+            colors.green /= max;
+            colors.blue /= max;
+            color = colors.toColor();
+
+            telemetry.addLine("normalized color:  ")
+                    .addData("a", "%02x", Color.alpha(color))
+                    .addData("r", "%02x", Color.red(color))
+                    .addData("g", "%02x", Color.green(color))
+                    .addData("b", "%02x", Color.blue(color));
+            telemetry.update();
+
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV(Color.red(color), Color.green(color), Color.blue(color), hsvValues);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0xff, values));
+                }
+            });
+        }
+    }
+}
